c52d0013b85070221b83e29f4215863c164ea397
JonatanRek <gamerclassn7@gmail.com>
2023-06-25 22:12:58 +0200

    Body Preparation function

--- app.ps1
+++ app.ps1
@@ -1,1 +1,23 @@
 
+function Get-FormatedEmailForHandler {
+    param (
+        [Parameter(Mandatory = $true)]
+        [mailaddress]$from,
+        [Parameter(Mandatory = $true)]
+        [mailaddress]$to,
+        [Parameter(Mandatory = $true)]
+        [string]$subject,
+        [Parameter(Mandatory = $true)]
+        [string]$body
+    )
+
+    $content = ""
+
+    $content += ("From: {0}`n" -f $from.Address)
+    $content += ("To: {0}`n" -f $to.Address)
+    $content += ("Subject: {0}`n" -f $subject)
+
+    $content += $body
+
+    return $content
+}
