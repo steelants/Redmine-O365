212ab78991c8b4728960eb75ab0fab35012db77e
JonatanRek <gamerclassn7@gmail.com>
2023-06-25 22:16:48 +0200

    Error HTTP Codes for later Translation

--- app.ps1
+++ app.ps1
@@ -21,3 +21,26 @@ function Get-FormatedEmailForHandler {
 
     return $content
 }
+
+# ERROR Messages
+# case response.code.to_i
+#       when 403
+#         warn "Request was denied by your Redmine server. " +
+#              "Make sure that 'WS for incoming emails' is enabled in application settings and that you provided the correct API key."
+#         return 77
+#       when 422
+#         warn "Request was denied by your Redmine server. " +
+#              "Possible reasons: email is sent from an invalid email address or is missing some information."
+#         return 77
+#       when 400..499
+#         warn "Request was denied by your Redmine server (#{response.code})."
+#         return 77
+#       when 500..599
+#         warn "Failed to contact your Redmine server (#{response.code})."
+#         return 75
+#       when 201
+#         debug "Processed successfully"
+#         return 0
+#       else
+#         return 1
+
